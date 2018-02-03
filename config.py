a2f830dd222c60652e08c035a317cf77ce0dcdd5
lapinskim <lapinski.maciej@gmail.com>
2018-02-04 00:39:29 +0100

    Forms implemented

--- config.py
+++ config.py
@@ -8,3 +8,6 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'webui.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # limit the maximum allowed payload
+    MAX_CONTENT_LENGTH = 8 * 1024 * 1024
+    GENE2CSM_THREADS = 3
--- webui/routes.py
+++ webui/routes.py
@@ -1,9 +1,12 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 from webui import app
+from webui.forms import InputForm
 
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-    page_content = "Hello world!"
-    return render_template('index.html', content=page_content)
+    form = InputForm()
+    if form.validate_on_submit():
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form)
